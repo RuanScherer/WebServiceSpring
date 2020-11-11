216ac755e473491240aabe3ec57729a796b9f5f4
Ruan Scherer <ruan.vscherer@gmail.com>
2020-11-11 08:36:29 -0300

    Init project

---
+++ src/test/java/com/ruanscherer/course/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruanscherer.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
