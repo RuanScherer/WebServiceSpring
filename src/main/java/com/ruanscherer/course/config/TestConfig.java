8094bc41f833f284c0b33f1cc89c37441ce1b382
Ruan Scherer <ruan.vscherer@gmail.com>
2020-11-11 19:08:50 -0300

    Create control for products + categories

--- src/main/java/com/ruanscherer/course/config/TestConfig.java
+++ src/main/java/com/ruanscherer/course/config/TestConfig.java
@@ -55,5 +55,14 @@ public class TestConfig implements CommandLineRunner {
         Product product4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product product5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
         productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
+
+        product1.getCategories().add(category2);
+        product2.getCategories().add(category1);
+        product2.getCategories().add(category3);
+        product3.getCategories().add(category3);
+        product4.getCategories().add(category3);
+        product5.getCategories().add(category2);
+
+        productRepository.saveAll((Arrays.asList(product1, product2, product3, product4, product5)));
     }
 }
