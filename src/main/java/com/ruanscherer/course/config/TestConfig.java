9c1f8da61d2ac499188336bd754a4aeec935a1a3
Ruan Scherer <ruan.vscherer@gmail.com>
2020-11-11 21:17:12 -0300

    Add Order Items

--- src/main/java/com/ruanscherer/course/config/TestConfig.java
+++ src/main/java/com/ruanscherer/course/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.ruanscherer.course.config;
 
-import com.ruanscherer.course.entities.Category;
-import com.ruanscherer.course.entities.Order;
-import com.ruanscherer.course.entities.Product;
-import com.ruanscherer.course.entities.User;
+import com.ruanscherer.course.entities.*;
 import com.ruanscherer.course.entities.enums.OrderStatus;
-import com.ruanscherer.course.repositories.CategoryRepository;
-import com.ruanscherer.course.repositories.OrderRepository;
-import com.ruanscherer.course.repositories.ProductRepository;
-import com.ruanscherer.course.repositories.UserRepository;
+import com.ruanscherer.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -64,5 +61,12 @@ public class TestConfig implements CommandLineRunner {
         product5.getCategories().add(category2);
 
         productRepository.saveAll((Arrays.asList(product1, product2, product3, product4, product5)));
+
+        OrderItem orderItem1 = new OrderItem(order1, product1, 2, product1.getPrice());
+        OrderItem orderItem2 = new OrderItem(order1, product3, 1, product3.getPrice());
+        OrderItem orderItem3 = new OrderItem(order2, product3, 2, product3.getPrice());
+        OrderItem orderItem4 = new OrderItem(order3, product5, 2, product5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(orderItem1, orderItem2, orderItem3, orderItem4));
     }
 }
---
+++ src/main/java/com/ruanscherer/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.ruanscherer.course.repositories;
+
+import com.ruanscherer.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
