b53f17effef938c9701c282d77e07ddd2b003ede
Ruan Scherer <ruan.vscherer@gmail.com>
2020-11-12 11:29:40 -0300

    Add payment entity and seed

--- src/main/java/com/ruanscherer/course/config/TestConfig.java
+++ src/main/java/com/ruanscherer/course/config/TestConfig.java
@@ -68,5 +68,9 @@ public class TestConfig implements CommandLineRunner {
         OrderItem orderItem4 = new OrderItem(order3, product5, 2, product5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(orderItem1, orderItem2, orderItem3, orderItem4));
+
+        Payment payment1 = new Payment(null, Instant.parse("2019-06-20T22:53:07Z"), order1);
+        order1.setPayment(payment1);
+        orderRepository.save(order1);
     }
 }
